d888a7bcea0e5d525d79ce995fa0c1eaed544517
suhang56 <suhang56@foxmail.com>
2026-04-03 04:46:29 -0400

    fix: replace broken Wikimedia aircraft photo URLs with verified ones
    All 21 aircraft photo URLs were returning 404 — original filenames
    did not exist on Wikimedia Commons. Replaced with verified filenames
    found via Wikimedia API search, using stable Special:FilePath redirect
    format that survives thumbnail path restructuring.

--- app/src/main/java/com/flightlog/app/data/network/AircraftTypePhotoProvider.kt
+++ app/src/main/java/com/flightlog/app/data/network/AircraftTypePhotoProvider.kt
@@ -97,110 +97,114 @@ object AircraftTypePhotoProvider {
      * Curated aircraft photos from Wikimedia Commons (freely licensed).
      * Each entry maps a canonical ICAO type code to a photo URL, photographer credit,
      * and human-readable aircraft name.
+     *
+     * URLs use the Special:FilePath redirect format for stability:
+     * https://commons.wikimedia.org/wiki/Special:FilePath/Filename.jpg?width=1280
+     * This avoids breakage from thumbnail path changes on Wikimedia servers.
      */
     private val AIRCRAFT_PHOTOS: Map<String, AircraftPhotoInfo> = mapOf(
         "B738" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Ryanair_B737-8AS_EI-DWO.jpg/1280px-Ryanair_B737-8AS_EI-DWO.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Gear_Actuation-Boeing-737-800_EL-AL_approaching_VIE-DSC_3259w.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Boeing 737-800"
         ),
         "B38M" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Southwest_Airlines_Boeing_737_MAX_8_N8713M_approaching_Washington_Reagan_Airport.jpg/1280px-Southwest_Airlines_Boeing_737_MAX_8_N8713M_approaching_Washington_Reagan_Airport.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Berlin_Brandenburg_Airport_Icelandair_Boeing_737-8_MAX_TF-ICY_(DSC07832).jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Boeing 737 MAX 8"
         ),
         "B744" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/British_Airways_Boeing_747-400_G-BNLY.jpg/1280px-British_Airways_Boeing_747-400_G-BNLY.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Thai_Airways_International_Boeing_747-4D7_HS-TGP_MUC_2015_03.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Boeing 747-400"
         ),
         "B752" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Delta_Air_Lines_Boeing_757-232_N665DN.jpg/1280px-Delta_Air_Lines_Boeing_757-232_N665DN.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Delta_Boeing_757-200_N699DL_BWI_MD1.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Boeing 757-200"
         ),
         "B762" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Delta_Air_Lines_Boeing_767-332_N130DL.jpg/1280px-Delta_Air_Lines_Boeing_767-332_N130DL.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Ba_b767-300_g-bnwa_planform_arp.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Boeing 767"
         ),
         "B77W" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Emirates_Boeing_777-300ER_A6-EGZ_at_Schiphol_%28cropped%29.jpg/1280px-Emirates_Boeing_777-300ER_A6-EGZ_at_Schiphol_%28cropped%29.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Qatar_Boeing_777-300ER_A7-BEP_IAD_VA1.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Boeing 777-300ER"
         ),
         "B789" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/All_Nippon_Airways_Boeing_787-9_JA882A_approaching_Runway_16R_at_Narita_Airport.jpg/1280px-All_Nippon_Airways_Boeing_787-9_JA882A_approaching_Runway_16R_at_Narita_Airport.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Etihad_Boeing_787-9_A6-BNI_IAD_VA2.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Boeing 787-9 Dreamliner"
         ),
         "A319" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Airbus_A319-112%2C_American_Airlines_AN2334797.jpg/1280px-Airbus_A319-112%2C_American_Airlines_AN2334797.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Finnair_Airbus_A319-112_OH-LVL_snowfall.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Airbus A319"
         ),
         "A320" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/A320-214_-_EasyJet_-_G-EZWB_-_LEMD.jpg/1280px-A320-214_-_EasyJet_-_G-EZWB_-_LEMD.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Lufthansa_Airbus_A320-211_D-AIQT_01.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Airbus A320"
         ),
         "A321" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Airbus_A321-231%2C_Air_France_AN1990552.jpg/1280px-Airbus_A321-231%2C_Air_France_AN1990552.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/American_Airbus_A321_N913US_BWI_MD1.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Airbus A321"
         ),
         "A333" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Airbus_A330-302%2C_Cathay_Pacific_Airways_JP7309806.jpg/1280px-Airbus_A330-302%2C_Cathay_Pacific_Airways_JP7309806.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/China_Eastern_Airlines_A330-300_B-6097_SVO_2011-6-17.png?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Airbus A330-300"
         ),
         "A343" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Lufthansa_A340-313X_D-AIGP.jpg/1280px-Lufthansa_A340-313X_D-AIGP.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/South_African_Airways_Airbus_A340-313_ZS-SXE_MUC_2015_02.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Airbus A340-300"
         ),
         "A35K" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Singapore_Airlines_Airbus_A350-941_%289V-SMF%29.jpg/1280px-Singapore_Airlines_Airbus_A350-941_%289V-SMF%29.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Airbus_A350-941_F-WWCF_MSN002_ILA_Berlin_2016_17.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Airbus A350"
         ),
         "A388" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/A6-EDY_A380_Emirates_31_jan_2013_jfk_%288442269364%29_%28cropped%29.jpg/1280px-A6-EDY_A380_Emirates_31_jan_2013_jfk_%288442269364%29_%28cropped%29.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Emirates_Airbus_A380-861_A6-EER_MUC_2015_01.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Airbus A380-800"
         ),
         "E190" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/JetBlue_Airways_Embraer_ERJ-190_N192JB.jpg/1280px-JetBlue_Airways_Embraer_ERJ-190_N192JB.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Hannover_Airport_Helvetic_Airways_Embraer_E190-E2_HB-AZE_(DSC09329).jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Embraer E190"
         ),
         "E195" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Azul_Brazilian_Airlines_Embraer_195_PR-AXH.jpg/1280px-Azul_Brazilian_Airlines_Embraer_195_PR-AXH.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Embraer_E195-E2,_Air_Show_2019,_Le_Bourget_(SIAE0894).jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Embraer E195"
         ),
         "CRJ9" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Bombardier_CRJ-900_Lufthansa_CityLine_D-ACNR.jpg/1280px-Bombardier_CRJ-900_Lufthansa_CityLine_D-ACNR.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Bombardier_CRJ-900_by_Lufthansa_CityLine.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "Bombardier CRJ-900"
         ),
         "AT76" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/ATR_72-600_HOP%21_%28F-HOPX%29.jpg/1280px-ATR_72-600_HOP%21_%28F-HOPX%29.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Cebgo_(Cebu_Pacific)_ATR_72-600.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "ATR 72-600"
         ),
         "DH8D" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Bombardier_Dash_8_Q400_%28All_Nippon_Airways%29.jpg/1280px-Bombardier_Dash_8_Q400_%28All_Nippon_Airways%29.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Austrian_Airlines_Bombardier_Dash_8_Q400_(flight_683)_boarding_at_Vienna_Airport,_Austria_(DSC_0003).jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "De Havilland Dash 8-400"
         ),
         "C919" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/B-919A_at_PKX_%2820220724162907%29.jpg/1280px-B-919A_at_PKX_%2820220724162907%29.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/COMAC_B-001A_May_2017.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "COMAC C919"
         ),
         "ARJ2" to AircraftPhotoInfo(
-            photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Chengdu_Airlines_ARJ21-700_at_CTU_%28cropped%29.jpg/1280px-Chengdu_Airlines_ARJ21-700_at_CTU_%28cropped%29.jpg",
+            photoUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/Comac_ARJ21-700.jpg?width=1280",
             photographer = "Wikimedia Commons",
             aircraftName = "COMAC ARJ21"
         )
